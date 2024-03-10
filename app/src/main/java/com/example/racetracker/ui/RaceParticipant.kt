b7fd51f217d741948dea914e0d788e0e4e9333b7
paangulo6 <paulalardero581@gmail.com>
2024-03-11 00:38:32 +0100

    Prueba

--- app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -35,6 +35,7 @@ class RaceParticipant(
         require(progressIncrement > 0) { "progressIncrement=$progressIncrement; must be > 0" }
     }
 
+
     /**
      * Indicates the race participant's current progress
      */
